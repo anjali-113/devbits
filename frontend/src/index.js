b4e90119f141f1aae8832baf214d92ccea56ca7f
anjali-113 <127101288+anjali-113@users.noreply.github.com>
2023-03-31 18:06:20 +0530

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Show from './pages/Home';
+import "./style.scss";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="/:id" element={<Show />}/>
+    </Routes>
+    </BrowserRouter>
+);
+
