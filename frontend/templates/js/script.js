0360d5d00925191bdf768967e79345bd23dceda0
anjali-113 <127101288+anjali-113@users.noreply.github.com>
2023-03-31 19:44:19 +0530

    Add files via upload

---
+++ frontend/templates/js/script.js
@@ -0,0 +1,91 @@
+/////////////////////////////////////////////////
+// Set current year
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
+/////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+////////////////////////////////////////////////
+// Smooth scrolling animation
+
+// const allLinks = document.querySelectorAll("a:link");
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+//     //Scroll back to top
+//     if (href === "#") {
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+//     }
+
+//     // Scroll to other links
+//     if (href != "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({
+//         behavior: "smooth",
+//       });
+//     }
+
+//     // Close the mobile navigation
+//     if (link.classList.contains("main-nav-link")) {
+//       headerEl.classList.toggle("nav-open");
+//     }
+//   });
+// });
+
+////////////////////////////////////////////////
+// Sticky header
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 720) {
+    this.document.body.classList.add("sticky");
+  } else {
+    this.document.body.classList.remove("sticky");
+  }
+});
+
+///////////////////////////////////////////////////
+//Testimonials
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
